e19dfa1edd53790a8fe186b15ebd6c170819ab8a
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2016-12-01 15:06:14 +0800

    Made it ready for PWA copying hoverboard
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

---
+++ scripts/elements.js
@@ -0,0 +1,11 @@
+UXPHCONF2017.Elements = (function () {
+  'use strict';
+
+  function init() {
+    UXPHCONF2017.Elements.Template = document.getElementById('uxphconf-app');
+  }
+
+  return {
+    init: init
+  };
+}());
